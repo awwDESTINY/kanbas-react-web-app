6f5ce50c942c4ac115c17b6c49bc60f9e3b27579
Zidao Wang <zidaowang@ZidaodeMacBook-Pro.local>
2024-06-16 19:18:57 -0400

    a6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,6 +7,8 @@ import * as client from "./Courses/client";
 import { useState, useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
+import Account from "./Account";
+import ProtectedRoute from "./ProtectedRoute";
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
   const fetchCourses = async () => {
@@ -50,16 +52,16 @@ export default function Kanbas() {
         <div className="flex-fill p-4">
           <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
-            <Route path="Dashboard" element={
+            <Route path="/Account/*" element={<Account />} />
+            <Route path="Dashboard" element={<ProtectedRoute>
             <Dashboard
               courses={courses}
               course={course}
               setCourse={setCourse}
               addNewCourse={addNewCourse}
               deleteCourse={deleteCourse}
-              updateCourse={updateCourse}/>} />
-            <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+              updateCourse={updateCourse}/></ProtectedRoute>} />
+            <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
             <Route path="Calendar" element={<h1>Calendar</h1>} />
             <Route path="Inbox" element={<h1>Inbox</h1>} />
           </Routes>
--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modules from "./Courses/Modules/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
+import accountReducer from "./Account/reducer";
 const store = configureStore({
   reducer: {
     modules,
     assignments: assignmentsReducer,
+    accountReducer,
   },
 });
 export default store;
