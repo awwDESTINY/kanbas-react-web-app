13d5bb06a82086ded19518729f2ec6a06d3d31ee
Zidao Wang <zidaowang@ZidaodeMacBook-Pro.local>
2024-06-18 20:13:19 -0400

    a6

--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -10,12 +10,15 @@ export default function Signup() {
   const dispatch = useDispatch();
   const signup = async () => {
     try {
-        const currentUser = await client.signup(user);
-        dispatch(setCurrentUser(currentUser));
-        navigate("/Kanbas/Account/Profile");
-    } catch (err: any) {
-        setError(err.response.data.message);
-    }
+        if (user.dob) {
+                user.dob = new Date(user.dob).toISOString();
+            }
+            const currentUser = await client.signup(user);
+            dispatch(setCurrentUser(currentUser));
+            navigate("/Kanbas/Account/Profile");
+        } catch (err: any) {
+            setError(err.response.data.message);
+        }
 
   };
   return (
@@ -26,6 +29,20 @@ export default function Signup() {
              className="form-control mb-2" placeholder="username" />
       <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} type="password"
              className="form-control mb-2" placeholder="password" />
+      <input value={user.firstName || ""} onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+             className="form-control mb-2" placeholder="first name"/>
+      <input value={user.lastName || ""} onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+             className="form-control mb-2" placeholder="last name"/>
+      <input value={user.email || ""} onChange={(e) => setUser({ ...user, email: e.target.value })}
+             type="email" className="form-control mb-2" placeholder="email"/>
+      <input value={user.dob || ""} onChange={(e) => setUser({ ...user, dob: e.target.value })}
+             type="date" className="form-control mb-2" placeholder="date of birth"/>
+      <select value={user.role || ""} onChange={(e) => setUser({ ...user, role: e.target.value })}
+              className="form-control mb-2">
+                <option value="STUDENT">STUDENT</option>
+                <option value="TA">ASSITANT</option>
+                <option value="FACULTY">FACULTY</option>
+      </select>
       <button onClick={signup} className="btn btn-primary mb-2"> Sign up </button><br />
       <Link to="/Kanbas/Account/Signin">Sign in</Link>
     </div>
